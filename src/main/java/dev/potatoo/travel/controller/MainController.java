de80481d86b4239f8d99a1324d0bd35a7c0363dc
jungu0416 <junguYY0809@gmail.com>
2022-04-13 14:04:40 +0900

    requestbody

--- src/main/java/dev/potatoo/travel/controller/MainController.java
+++ src/main/java/dev/potatoo/travel/controller/MainController.java
@@ -8,9 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.potatoo.travel.model.Location;
 import dev.potatoo.travel.service.TravelService;
 
 @RestController
@@ -34,10 +34,9 @@ public class MainController {
 	}
 	
 	@PostMapping("/update/area")
-	public ResponseEntity<Object> updateArea(@RequestBody String location){
+	public ResponseEntity<Object> updateArea(@RequestBody Location location){
+		System.out.println("location : " + location.getLocation());
 		
-		System.out.println("location : " + location);
-		
-		return new ResponseEntity<>(travelService.updateArea(location), HttpStatus.OK);
+		return new ResponseEntity<>(travelService.updateArea(location.getLocation()), HttpStatus.OK);
 	}
 }
---
+++ src/main/java/dev/potatoo/travel/model/Location.java
@@ -0,0 +1,8 @@
+package dev.potatoo.travel.model;
+
+import lombok.Data;
+
+@Data
+public class Location {
+	private String location;
+}
