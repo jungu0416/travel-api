78c8dd05b8765ff813d07f1e54176d3f968abcbc
DESKTOP-I9F93C9\Jungu <jungu0416@naver.com>
2023-03-12 21:28:59 +0900

    DB 변경

--- src/main/java/dev/potatoo/travel/controller/MainController.java
+++ src/main/java/dev/potatoo/travel/controller/MainController.java
@@ -3,20 +3,18 @@ package dev.potatoo.travel.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import dev.potatoo.travel.model.Location;
 import dev.potatoo.travel.service.TravelService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class MainController {
 	@Autowired
 	private TravelService travelService;
-	
+
 	@PostMapping("/check/area")
 	public ResponseEntity<Object> checkArea() {
 		return new ResponseEntity<>(travelService.checkArea(),HttpStatus.OK);
